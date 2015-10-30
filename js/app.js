db699b1d10e82af54f84dce9e29e6b5b146738a7
davidcolacilli <davidcolacilli@users.noreply.github.com>
2015-10-30 19:08:10 -0300

    gallery

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@
   };
   
   gems = [
-    { name: 'Azurite', price: 2.95, canPurchase: true},
-    { name: 'Bloodstone', price: 5.95, canPurchase: true},
-    { name: 'Zircon', price: 3.95, canPurchase: false}
+    { name: 'Azurite', price: 2.95, images: ['img/gem-01.png','img/gem-02.png','img/gem-03.png'], canPurchase: true},
+    { name: 'Bloodstone', price: 5.95, images: ['img/gem-04.png','img/gem-05.png','img/gem-06.png'], canPurchase: true},
+    { name: 'Zircon', price: 3.95, images: ['img/gem-07.png','img/gem-08.png','img/gem-09.png'], canPurchase: false}
   ];
 })();
